825a28dab3558980889ff4a0a5a414dc76847924
Janne Julkunen <janne.julkunen@zef.fi>
2020-04-14 22:36:05 +0300

    Added missing events for the component

--- projects/lib/src/lib/fabric.directive.ts
+++ projects/lib/src/lib/fabric.directive.ts
@@ -46,53 +46,53 @@ export class FabricDirective implements OnInit, OnDestroy, DoCheck, OnChanges {
   @Input('fabric') config?: FabricConfigInterface;
 
   @Output() drop = new EventEmitter<any>();
-    @Output() dragover = new EventEmitter<any>();
-    @Output() dragenter = new EventEmitter<any>();
-    @Output() dragleave = new EventEmitter<any>();
+  @Output() dragover = new EventEmitter<any>();
+  @Output() dragenter = new EventEmitter<any>();
+  @Output() dragleave = new EventEmitter<any>();
 
-    @Output() mouseup = new EventEmitter<any>();
-    @Output() mousedown = new EventEmitter<any>();
-    @Output() mouseover = new EventEmitter<any>();
-    @Output() mouseout = new EventEmitter<any>();
-    @Output() mousemove = new EventEmitter<any>();
-    @Output() mousewheel = new EventEmitter<any>();
-    @Output() mousedblclick = new EventEmitter<any>();
-    @Output() mouseupBefore = new EventEmitter<any>();
-    @Output() mousedownBefore = new EventEmitter<any>();
-    @Output() mousemoveBefore = new EventEmitter<any>();
+  @Output() mouseup = new EventEmitter<any>();
+  @Output() mousedown = new EventEmitter<any>();
+  @Output() mouseover = new EventEmitter<any>();
+  @Output() mouseout = new EventEmitter<any>();
+  @Output() mousemove = new EventEmitter<any>();
+  @Output() mousewheel = new EventEmitter<any>();
+  @Output() mousedblclick = new EventEmitter<any>();
+  @Output() mouseupBefore = new EventEmitter<any>();
+  @Output() mousedownBefore = new EventEmitter<any>();
+  @Output() mousemoveBefore = new EventEmitter<any>();
 
-    @Output() mouseUp = new EventEmitter<any>();
-    @Output() mouseDown = new EventEmitter<any>();
-    @Output() mouseOver = new EventEmitter<any>();
-    @Output() mouseOut = new EventEmitter<any>();
-    @Output() mouseMove = new EventEmitter<any>();
-    @Output() mouseDblclick = new EventEmitter<any>();
-    @Output() mouseUpBefore = new EventEmitter<any>();
-    @Output() mouseDownBefore = new EventEmitter<any>();
-    @Output() mouseMoveBefore = new EventEmitter<any>();
+  @Output() mouseUp = new EventEmitter<any>();
+  @Output() mouseDown = new EventEmitter<any>();
+  @Output() mouseOver = new EventEmitter<any>();
+  @Output() mouseOut = new EventEmitter<any>();
+  @Output() mouseMove = new EventEmitter<any>();
+  @Output() mouseDblclick = new EventEmitter<any>();
+  @Output() mouseUpBefore = new EventEmitter<any>();
+  @Output() mouseDownBefore = new EventEmitter<any>();
+  @Output() mouseMoveBefore = new EventEmitter<any>();
 
-    @Output() pathCreated = new EventEmitter<any>();
-    @Output() alterRender = new EventEmitter<any>();
+  @Output() pathCreated = new EventEmitter<any>();
+  @Output() alterRender = new EventEmitter<any>();
 
-    @Output() objectAdded = new EventEmitter<any>();
-    @Output() objectMoved = new EventEmitter<any>();
-    @Output() objectScaled = new EventEmitter<any>();
-    @Output() objectSkewed = new EventEmitter<any>();
-    @Output() objectRotated = new EventEmitter<any>();
-    @Output() objectRemoved = new EventEmitter<any>();
-    @Output() objectModified = new EventEmitter<any>();
-    @Output() objectSelected = new EventEmitter<any>();
-    @Output() objectMoving = new EventEmitter<any>();
-    @Output() objectScaling = new EventEmitter<any>();
-    @Output() objectSkewing = new EventEmitter<any>();
-    @Output() objectRotating = new EventEmitter<any>();
+  @Output() objectAdded = new EventEmitter<any>();
+  @Output() objectMoved = new EventEmitter<any>();
+  @Output() objectScaled = new EventEmitter<any>();
+  @Output() objectSkewed = new EventEmitter<any>();
+  @Output() objectRotated = new EventEmitter<any>();
+  @Output() objectRemoved = new EventEmitter<any>();
+  @Output() objectModified = new EventEmitter<any>();
+  @Output() objectSelected = new EventEmitter<any>();
+  @Output() objectMoving = new EventEmitter<any>();
+  @Output() objectScaling = new EventEmitter<any>();
+  @Output() objectSkewing = new EventEmitter<any>();
+  @Output() objectRotating = new EventEmitter<any>();
 
-    @Output() selectionCleared = new EventEmitter<any>();
-    @Output() selectionCreated = new EventEmitter<any>();
-    @Output() selectionUpdated = new EventEmitter<any>();
+  @Output() selectionCleared = new EventEmitter<any>();
+  @Output() selectionCreated = new EventEmitter<any>();
+  @Output() selectionUpdated = new EventEmitter<any>();
 
-    @Output() beforeTransform = new EventEmitter<any>();
-    @Output() beforeSelectionCleared = new EventEmitter<any>();
+  @Output() beforeTransform = new EventEmitter<any>();
+  @Output() beforeSelectionCleared = new EventEmitter<any>();
 
   constructor(private zone: NgZone,
     private elementRef: ElementRef, private differs: KeyValueDiffers,
